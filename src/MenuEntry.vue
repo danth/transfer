22c525292f3a3d42952874b8c54b031e929b0bf9
Daniel Thwaites <danthwaites30@btinternet.com>
2023-04-06 17:13:03 +0100

    Fix icon colour in dark mode :bug:

--- src/MenuEntry.vue
+++ src/MenuEntry.vue
@@ -1,11 +1,9 @@
 <template>
   <li>
     <a class="menuitem" @click.stop="open">
-      <svg class="svg icon" width="44" height="44" viewBox="0 0 44 44">
-        <image
-          x="12" y="12" width="20" height="20"
-          preserveAspectRatio="xMinYMin meet"
-          :xlink:href="icon" />
+      <svg class="svg icon" viewBox="0 0 10 10">
+        <path fill="currentColor" d="M 2.6359711,1.5625117 0.89397505,3.3045077 2.6359711,5.0465033 V 4.0018944 H 8.0476922 V 3.3045077 2.6071206 H 2.6359711 Z" />
+        <path fill="currentColor" d="M 7.3640303,4.9534972 9.106025,6.6954927 7.3640303,8.4374883 V 7.3928799 H 1.9523082 V 6.6954927 5.9981059 h 5.4117221 z" />
       </svg>
 
       <span class="displayname">
@@ -19,19 +17,12 @@
 
 <script>
   import Vue from 'vue'
-  import { generateFilePath } from '@nextcloud/router'
   import Transfer from './Transfer.vue'
 
   export default {
     components: { Transfer },
 
     props: ['currentDirectory'],
-
-    computed: {
-      icon() {
-        return generateFilePath('transfer', 'img', 'app-dark.svg')
-      }
-    },
 
     methods: {
       open() {
@@ -42,3 +33,10 @@
   }
 </script>
 
+<style scoped>
+svg {
+  width: 16px;
+  height: 16px;
+  margin: 14px;
+}
+</style>
